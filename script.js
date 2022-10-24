6d4dde381761d6da20c389f7f9c7b9a6278ef458
Carlos Xavier <carlos.lipe559@hotmail.com>
2022-10-24 19:02:47 -0300

    add langs

--- script.js
+++ script.js
@@ -23,24 +23,3 @@ function faq() {
 }
 
 faq();
-
-/*
-function faq() {
-
-    const pergunta = document.querySelectorAll(".faq");
-    const resposta = document.querySelectorAll(".faq_text");
-   
-    for(let i = 0; i <= pergunta.length; i++) {
-
-        pergunta[i].addEventListener("click", () => {
-            if(  )
-
-        });
-      
-
-    }
-
-
-}
-
-faq();*/
